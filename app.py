518b7bd5ba227646151a91f0ecb769d27231b9f9
Mike-Brown5 <mikeaxe5@outlook.com>
2022-05-07 18:07:49 +0200

    done with the password generator

---
+++ app.py
@@ -0,0 +1,20 @@
+import random
+from turtle import up
+
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+lowercase = uppercase.lower()
+
+numbers = "0123456789"
+
+symbols = "~!@#$%^&*()_<>"
+
+password = uppercase + lowercase + numbers+symbols
+
+length = input("Enter the lenght of your password : ")
+
+create = random.sample(password, int(length))
+
+create_pass = ''.join(str(i) for i in create)
+
+print(f"Your password is : {create_pass}")
